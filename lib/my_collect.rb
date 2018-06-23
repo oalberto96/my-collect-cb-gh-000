141c291ad0842d7bd668c978e591e5a84b136601
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 00:17:19 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,10 +4,9 @@ def my_collect(array)
   new_array = []
   i = 0
   while i < array.size
-    new_array << array[]
+    new_array << array[i]
     i += 1
   end
+  puts new_array
   new_array
 end
-
-
